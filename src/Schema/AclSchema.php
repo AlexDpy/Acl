7e0cadc4bb716f41dcca7f9d4efb9d2f6d58dc48
Alexandre Dupuy <alex.dpy@gmail.com>
2015-08-21 00:10:07 +0200

    added possibility to customize database schema

--- src/Schema/AclSchema.php
+++ src/Schema/AclSchema.php
@@ -2,32 +2,34 @@
 
 namespace AlexDpy\Acl\Schema;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Schema\SchemaConfig;
 use Doctrine\DBAL\Types\Type;
 
 class AclSchema extends Schema
 {
-    const REQUESTER_LENGTH = 255;
-    const RESOURCE_LENGTH = 255;
+    /**
+     * @param array           $options
+     * @param Connection|null $connection
+     */
+    public function __construct(array $options = [], Connection $connection = null) {
+        parent::__construct([], [], null === $connection ? null : $connection->getSchemaManager()->createSchemaConfig());
 
-    public function __construct(
-        array $tables = array(),
-        array $sequences = array(),
-        SchemaConfig $schemaConfig = null,
-        array $namespaces = array()
-    ) {
-        parent::__construct($tables, $sequences, $schemaConfig, $namespaces);
+        $options = array_merge([
+            'permissions_table_name' => 'acl_permissions',
+            'requester_column_length' => 255,
+            'resource_column_length' => 255,
+        ], $options);
 
-        $permissionsTable = $this->createTable('acl_permissions');
-        $permissionsTable->addColumn('requester', Type::STRING, ['length' => self::REQUESTER_LENGTH]);
-        $permissionsTable->addColumn('resource', Type::STRING, ['length' => self::RESOURCE_LENGTH]);
+        $permissionsTable = $this->createTable($options['permissions_table_name']);
+
+        $permissionsTable->addColumn('requester', Type::STRING, ['length' => $options['requester_column_length']]);
+        $permissionsTable->addColumn('resource', Type::STRING, ['length' => $options['resource_column_length']]);
         $permissionsTable->addColumn('mask', Type::INTEGER);
+
         $permissionsTable
             ->setPrimaryKey(['requester', 'resource'])
             ->addUniqueIndex(['resource', 'requester'])
             ->addIndex(['resource', 'requester']);
     }
-
 }
- 
